fc58ac140d182dacd8bf24cbb46447fd56e6f96a
Gustavo Viana Neves <gus.viana1@outlook.com>
2026-09-08 10:48:45 -0300

    adicionadas classes de security

---
+++ src/main/java/com/javanauta/usuario/infrastructure/entity/Telefone.java
@@ -0,0 +1,26 @@
+package com.javanauta.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "telefone")
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "ddd", length = 3)
+    private String ddd;
+}
